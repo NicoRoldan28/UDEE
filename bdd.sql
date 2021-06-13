cd2a96acbc3bfb5866c1758f65f91f26f81454c3
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-13 16:54:23 -0300

    FEDE-BD

--- bdd.sql
+++ bdd.sql
@@ -196,3 +196,260 @@ Por lo demás , esta bien , revisaría esas cosas, tabla de measurements
 ● Tipo de tarifa
 ● Total a pagar */
 
+-------------------------------------------------------------------------------------
+
+
+
+DELIMITER //
+CREATE PROCEDURE up_clients(new_id_address INT(11),new_name VARCHAR(30), new_last_name VARCHAR(30),new_email VARCHAR(40))
+BEGIN
+	INSERT INTO Clients(id_address,name,last_name,email)
+        VALUES(new_id_address,new_name,new_last_name,new_email);
+END //
+DELIMITER //
+CREATE PROCEDURE up_users(new_id_client INT(11), new_username VARCHAR(30), new_password VARCHAR(30))
+BEGIN
+	INSERT INTO Users(id_client, username, password)
+        VALUES (new_id_client,new_username,new_password);
+END //
+DELIMITER //
+CREATE PROCEDURE up_rates(new_price FLOAT)
+BEGIN
+	INSERT INTO Rates(price)
+        VALUES(new_price);
+END //
+DELIMITER //
+CREATE PROCEDURE up_address(new_id_rate INT(11),new_street VARCHAR(30), new_number INT(11))
+BEGIN
+	INSERT INTO Address(id_rate,street,number)
+        VALUES(new_id_rate,new_street,new_number);
+END //
+DELIMITER //
+CREATE PROCEDURE up_brands(new_name VARCHAR(30))
+BEGIN
+	INSERT INTO Brands(name)
+        VALUES(new_name);
+END //
+DELIMITER //
+CREATE PROCEDURE up_models(new_name VARCHAR(30))
+BEGIN
+	INSERT INTO Models(name)
+        VALUES(new_name);
+END//
+DELIMITER //
+CREATE PROCEDURE up_meters(new_id_address INT(11), new_serial_numbers VARCHAR(30), new_password VARCHAR(30), new_id_brand INT(11), new_id_model INT(11))
+BEGIN
+	INSERT INTO meters(id_address,serial_number,password,id_brand,id_model)
+        VALUES(new_id_address,new_serial_numbers,new_password,new_id_brand,new_id_model);
+END //
+DELIMITER //
+CREATE PROCEDURE up_measurements(new_id_bill INT(11),new_id_meter INT(11),new_measurement VARCHAR(30), new_date DATE)
+BEGIN
+	INSERT INTO Measurements(id_bill,id_meter,measurement,date)
+        VALUES(new_id_bill,new_id_meter,new_measurement,new_date);
+END //
+DELIMITER //
+CREATE PROCEDURE up_bills(new_id_bill INT(11), new_id_user INT(11), new_id_address INT(11), new_id_measurement INT(11), new_measure_start INT(11), new_measure_end INT(11), new_date_time_start datetime, new_date_time_end datetime, new_id_rate INT(11))
+BEGIN
+	DECLARE new_total DOUBLE;
+        DECLARE new_consumption_total DOUBLE;
+        SET new_consumption_total = (SELECT measurement FROM Measurements WHERE new_id_measurement = id_measurement);
+        SET new_total = new_consumption_total * (SELECT  price 
+		FROM Rates
+		WHERE id_rate = new_id_rate);
+        INSERT INTO Bills(id_bill,id_user,id_address,id_measurement,measure_start,measure_end,consumption_total,date_time_start,date_time_End,id_rate,total)
+	VALUES(new_id_bill,new_id_user,new_id_address,new_id_measurement,new_measure_start,new_measure_end,new_consumption_total,new_date_time_start,new_date_time_end,new_id_rate,new_total);
+END //
+-----------------------------------------------------------------------
+DELIMITER //
+CREATE PROCEDURE down_clients(new_id_client INT(11))
+BEGIN 
+        DELETE FROM Clients WHERE id_address = new_id_client;
+END //
+DELIMITER //
+CREATE PROCEDURE down_users(new_id_user INT(11))
+BEGIN 
+        DELETE FROM Users WHERE id_user = new_id_user;
+END //
+DELIMITER //
+CREATE PROCEDURE down_rates(new_id_rate INT(11))
+BEGIN 
+        DELETE FROM Rates WHERE id_rate = new_id_rate;
+END //
+DELIMITER //
+CREATE PROCEDURE down_address(new_id_address INT(11))
+BEGIN 
+        DELETE FROM Address WHERE id_address = new_id_address;
+END //
+DELIMITER //
+CREATE PROCEDURE down_brands(new_id_brand INT(11))
+BEGIN 
+        DELETE FROM Brands WHERE id_brand = new_id_brand;
+END //
+DELIMITER //
+CREATE PROCEDURE down_models(new_id_model INT(11))
+BEGIN 
+        DELETE FROM Models WHERE id_model = new_id_model;
+END //
+DELIMITER //
+CREATE PROCEDURE down_meters(new_id_meter INT(11))
+BEGIN 
+        DELETE FROM meters WHERE id_meter = new_id_meter;
+END //
+DELIMITER //
+CREATE PROCEDURE down_measurements(new_id_measurement INT(11))
+BEGIN 
+        DELETE FROM Measurements WHERE id_measurement = new_id_measurement;
+END //
+DELIMITER //
+CREATE PROCEDURE down_bills(new_id_bill INT(11))
+BEGIN 
+        DELETE FROM Bills WHERE id_bill = new_id_bill;
+END //
+
+-----------------------------------------------------------------------
+
+DELIMITER //
+CREATE PROCEDURE update_clients(new_id_client INT(11),new_id_address INT(11),new_name VARCHAR(30),new_last_name VARCHAR(30),new_email VARCHAR(40))
+BEGIN 
+        UPDATE Clients SET id_address = new_id_address,name = new_name,last_name = new_last_name,email = new_email WHERE id_client = new_id_client;
+END //
+DELIMITER //
+CREATE PROCEDURE update_users(new_id_user INT(11),new_id_client INT(11),new_username VARCHAR(30),new_password VARCHAR(30))
+BEGIN 
+        UPDATE Users SET id_client = new_id_client,username = new_username,password = new_password WHERE id_user = new_id_user;
+END //
+DELIMITER //
+CREATE PROCEDURE update_rates(new_id_rate INT(11),new_price FLOAT)
+BEGIN 
+        UPDATE Rates SET price = new_price WHERE id_rate = new_id_rate;
+END //
+DELIMITER //
+CREATE PROCEDURE update_address(new_id_address INT(11),new_id_rate INT(11),new_street VARCHAR(30),new_number INT(11))
+BEGIN 
+        UPDATE Address SET id_rate = new_id_rate,street = new_street,number = new_number WHERE id_address = new_id_address;
+END //
+DELIMITER //
+CREATE PROCEDURE update_brands(new_id_brand INT(11),new_name VARCHAR(30))
+BEGIN 
+        UPDATE Brands SET name = new_name WHERE id_brand = new_id_brand;
+END //
+DELIMITER //
+CREATE PROCEDURE update_models(new_id_model INT(11),new_name VARCHAR(30))
+BEGIN   
+        UPDATE Models SET name = new_name WHERE id_model = new_id_model;
+END //
+DELIMITER //
+CREATE PROCEDURE update_meters(new_id_meter INT(11),new_id_address INT(11),new_serial_number VARCHAR(30),new_password VARCHAR(30),new_id_brand INT(11),new_id_model INT(11))
+BEGIN 
+        UPDATE meters SET id_address = new_id_address,serial_number = new_serial_number,password = new_password,id_brand = new_id_brand,id_model = new_id_model WHERE id_meter = new_id_meter;
+END //
+DELIMITER //
+CREATE PROCEDURE update_measurements(new_id_measurement INT(11),new_id_bill INT(11),new_id_meter INT(11), new_measurement VARCHAR(30),new_date DATE)
+BEGIN 
+        UPDATE Measurements SET id_bill = new_id_bill,id_meter = new_id_meter,measurement = new_measurement,date = new_date WHERE id_measurement = new_id_measurement;
+END //
+DELIMITER //
+CREATE PROCEDURE update_bills(new_id_bill INT(11), new_id_user INT(11), new_id_address INT(11), new_id_measurement INT(11), new_measure_start INT(11), new_measure_end INT(11), new_date_time_start datetime, new_date_time_end datetime, new_id_rate INT(11))
+BEGIN 
+        DECLARE new_total DOUBLE;
+        DECLARE new_consumption_total DOUBLE;
+        SET new_consumption_total = (SELECT measurement FROM Measurements WHERE new_id_measurement = id_measurement);
+	SET new_total = new_consumption_total * (SELECT  price 
+			                                FROM Rates
+			                                 WHERE id_rate = new_id_rate);
+        UPDATE Bills 
+        SET id_user=new_id_user,id_address=new_id_address,id_measurement=new_id_measurement,measure_start=new_measure_start,measure_end=new_measure_end,consumption_total=new_consumption_total,date_time_start=new_date_time_start,date_time_End=new_date_time_end,id_rate=new_id_rate,total=new_total 
+        WHERE id_bill = new_id_bill;
+END //
+
+-----------------------------------------------------------------------
+
+DELIMITER //
+CREATE PROCEDURE select_clients(new_id_client INT(11))
+BEGIN 
+        SELECT C.id_client,C.name,C.last_name,C.email,U.id_user,U.username,U.password,A.id_address,A.street,A.number,R.id_rate,R.price,M.id_meter,M.serial_number,M.password,ME.id_measurement,ME.measurement,ME.date,B.id_bill,B.id_measurement,B.measure_start,B.measure_end,B.consumption_total,B.date_time_start,B.date_time_End,B.total
+            FROM Clients C 
+        INNER JOIN Address A 
+                ON C.id_address = A.id_address 
+        INNER JOIN Rates R 
+                ON R.id_rate = A.id_rate 
+        INNER JOIN meters M
+                ON M.id_address = A.id_address
+        INNER JOIN Measurements ME
+                ON ME.id_meter = M.id_meter
+        INNER JOIN Bills B
+                ON B.id_address = A.id_address
+        INNER JOIN Users U
+                ON U.id_client = C.id_client
+            WHERE C.id_client = new_id_client; 
+END //
+
+DELIMITER //
+CREATE PROCEDURE cal_total(IN new_id_bill INT (11))
+BEGIN 
+        DECLARE price2 DOUBLE;
+        DECLARE total2 INT;
+        SET price2 = (SELECT  price 
+                        FROM Rates R 
+                        INNER JOIN Bills B 
+                        ON R.id_rate = B.id_rate
+                        WHERE B.id_bill = new_id_bill);
+        SET total2 = (price2 * (SELECT consumption_total 
+                                        FROM Bills 
+                                        WHERE id_bill = new_id_bill));
+        SELECT total2;
+END //
+
+-----------------------------------------------------------------------
+
+DELIMITER //
+CREATE PROCEDURE select_all_address()
+BEGIN 
+        SELECT id_address,id_rate,street,number FROM Address;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_bills()
+BEGIN 
+        SELECT id_bill,id_address,id_measurement,measure_start,measure_end,consumption_total,date_time_start,date_time_End,id_rate,total FROM Bills;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_brands()
+BEGIN 
+        SELECT id_brand,name FROM Brands;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_clients()
+BEGIN 
+        SELECT id_client,id_address,name,last_name,email FROM Clients;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_measurements()
+BEGIN 
+        SELECT id_measurement,id_bill,id_meter,measurement,date FROM Measurements;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_meters()
+BEGIN 
+        SELECT id_meter,id_address,serial_number,password,id_brand,id_model FROM meters;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_models()
+BEGIN 
+        SELECT id_model,name FROM Models;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_rates()
+BEGIN 
+        SELECT id_rate,price FROM Rates;
+END //
+DELIMITER //
+CREATE PROCEDURE select_all_users()
+BEGIN 
+        SELECT id_user,id_client,username,password FROM Users;
+END //
+
+-----------------------------------------------------------------------
+
+
+
